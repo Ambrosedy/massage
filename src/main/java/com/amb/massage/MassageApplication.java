e1c1e5a0a27b362c53feea913e6698802eda1017
Ambrosedy <Ambrosedy@163.com>
2021-01-22 12:42:51 +0800

    Massage one

---
+++ src/main/java/com/amb/massage/MassageApplication.java
@@ -0,0 +1,13 @@
+package com.amb.massage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassageApplication.class, args);
+	}
+
+}
